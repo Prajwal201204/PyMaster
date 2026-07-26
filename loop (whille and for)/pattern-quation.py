1313598db93b4d3873e8b38b805122089685a660
Myuse <myuse@gmail.com>
2026-07-26 19:07:25 +0530

    new

--- loop (whille and for)/pattern-quation.py
+++ loop (whille and for)/pattern-quation.py
@@ -143,29 +143,24 @@
 ##############################################################################################################################3
 
 # # 1)
-# for i in range(1,6):
-#     for j in range(1,6):
-#         if j<=i:
-#             print("*",end='')
-#         else:
-#             print(' ',end='')
-#     print()
+for i in range(1,6):
+    for j in range(1,6):
+        if j<=i:
+            print("*",end='')
+        else:
+            print(' ',end='')
+    print()
 
 
 # # 2)
-# for i in range(1,6):
-#     for j in range(1,6):
-#         if i<=j:
-#             print("*",end='')
-#         else:
-#             print(' ',end='')
-#     print()
+for i in range(1,6):
+    for j in range(1,6):
+        if i<=j:
+            print("*",end='')
+        else:
+            print(' ',end='')
+    print()
 
-
-
-
-
-
-
+3
 
 
