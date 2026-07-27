470334372dfc46a6826a2d9f68eacb0e00ab5f35
Myuse <myuse@gmail.com>
2026-07-27 21:14:13 +0530

    new

--- loop (whille and for)/pattern-quation.py
+++ loop (whille and for)/pattern-quation.py
@@ -162,10 +162,12 @@
 #     print()
 
 
-for i in range(1,6):
-    for j in range (1,11):
-        if j>=6-i and j<=4+i:
-            print('*',end='')
-        else:
-            print(' ',end='')
-    print()
+
+# # 3)
+# for i in range(1,6):
+#     for j in range (1,11):
+#         if j>=6-i and j<=4+i:
+#             print('*',end='')
+#         else:
+#             print(' ',end='')
+#     print()
