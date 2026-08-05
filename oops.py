bccb4053a546c2a12ec3a3787e05551ad3b7f065
Myuse <myuse@gmail.com>
2026-08-05 20:39:56 +0530

    new

--- oops.py
+++ oops.py
@@ -25,14 +25,14 @@
 # print(ob2.a,ob2.b)
 
 
-# # #
-# class Python:
-#     a=50
-#     b=20
-# ob1=Python()
-# ob2=Python() 
-# ob1.a=40
-# ob1.b=35
-# Python.a=12
-# print(ob1.a,ob1.b)
-# print(ob2.a,ob2.b)
+# #
+class Python:
+    a=50
+    b=20
+ob1=Python()
+ob2=Python() 
+ob1.a=40
+ob1.b=35
+Python.a=12
+print(ob1.a,ob1.b)
+print(ob2.a,ob2.b)
